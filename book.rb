06e78ccf6facd496b95df7ad35f845fa6058cb0d
Frempong <samankwaa.frempong@gmail.com>
2023-04-27 13:18:41 +0000

    Created many-to-many relationship between Person and Book classes through the rental class

--- book.rb
+++ book.rb
@@ -1,9 +1,17 @@
+require './rental'
+require './person'
+
 class Book
   attr_accessor :rentals, :title, :author
   
   def initialize(title, author)
     @title = title
     @author = author
-    rentals = []
+    @rentals = []
   end
+
+  def add_rental(rental)
+    rentals.push(rental)
+  end
+
 end
--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_service?
@@ -19,6 +20,11 @@ class Person < Nameable
 
   def correct_name
     name
+  end
+
+  def add_rental(rental)
+    rentals.push(rental)
+    rental.person = self
   end
 
   private
