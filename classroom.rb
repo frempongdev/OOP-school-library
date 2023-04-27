ae3c545e3ee16ff598c5626a42c7c50d866cf7c1
Frempong <samankwaa.frempong@gmail.com>
2023-04-27 08:39:30 +0000

    Add Student belongs-to Classroom association to student class

--- classroom.rb
+++ classroom.rb
@@ -1,12 +1,24 @@
+require './student'
+
 class Classroom
   attr_accessor :label
+  attr_reader :students
   def initialize(label)
     @label = label
     @students = []
   end
-  
+
   def add_student(student)
     students.push(student)
     student.classroom = self
   end
 end
+
+#Tests
+# sc = Classroom.new('Science')
+# bb = Student.new(17, 'Obour')
+# tt = Student.new(21, 'Theo')
+# sc.add_student(bb)
+# sc.add_student(tt)
+# puts "#{tt.name} belongs to the #{tt.classroom.label} class"
+# puts "The #{sc.label} class has these students...", sc.students.map {|student| student.name}
--- student.rb
+++ student.rb
@@ -1,17 +1,26 @@
 require './person'
+# require './classroom'
 
 class Student < Person
-  def initialize(name, classroom, age)
-    super(name, age)
-    @classroom = classroom
-  end
+  attr_accessor :classroom
 
+  def initialize(age, name)
+    super(age, name)
+  end
+  
   def play_hooky
     '¯(ツ)/¯'
+  end
+  
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
 # Tests
-# Bb = Student.new(17, 'science', 'Obour')
+# Bb = Student.new(17, 'Obour')
+# Bb.classroom = 'Science'
 # puts "Name: #{Bb.name}, ID: #{Bb.id}, Age: #{Bb.age}"
 # puts "Plays #{Bb.play_hooky}"
+# puts "Belongs to the #{Bb.classroom} class"
