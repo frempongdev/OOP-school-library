1ca9f29ea91529226e8b56748b782d9df04acddf
Frempong <samankwaa.frempong@gmail.com>
2023-05-03 22:23:58 +0000

    Create converter class to convert hash to object

---
+++ converter.rb
@@ -0,0 +1,20 @@
+class BookConverter
+    attr_accessor :title, :author, :rentals
+    def initialize(title, author)
+        @title = title
+        @author =author
+        @rentals = []
+    end
+ end
+
+ class PersonConverter
+    attr_accessor :id, :age, :name, :rentals
+
+    
+    def initialize(id, age, name)
+        @id = id
+        @age = age
+        @name = name
+        @rentals = []
+    end
+ end
