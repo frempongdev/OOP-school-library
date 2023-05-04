34e668dd40224259d2d0b4505c64779dcd7bd3a2
jonas-45 <jonaswullah@gmail.com>
2023-05-04 11:59:21 +0000

    Add test for add rental method in person class

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../person'
+require_relative '../book'
 
 describe Person do
   context "testing initialize method of person class" do
@@ -24,6 +25,17 @@ describe Person do
       person = Person.new(33)
 
       expect(person.can_use_service?).to eq(true)
+    end
+  end
+
+  context "add rental method" do
+    it "should add a new rental" do
+      book = Book.new('Things fall apart','Chinua Achebe')
+      person = Person.new(33)
+      rental = person.add_rental(book,'2023')
+
+      expect(rental.book).to eq(book)
+      expect(rental.person.age).to eq(33)
     end
   end
 end
